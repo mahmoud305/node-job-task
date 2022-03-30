440e15bad8eedb65ed691d2e9fcdaff2ea1df7b6
mahmoud305 <ma5027300@gmail.com>
2022-03-30 16:32:20 +0200

    handling issues

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -1,7 +1,7 @@
 const  mongoose  = require("mongoose");
 
 const dataBaseConfig= ()=>{
-    mongoose.connect(process.env.MONGO_ATLAS_CONNECTION_LINK).
+    mongoose.connect(process.env.DATABASE_CONNECTION_LINK).
     then(console.log("DataBase connected successfully")).
     catch((error)=> console.log("error in dataBase Connection\n",error))
 };
