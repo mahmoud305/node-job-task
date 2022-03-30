c928594cd2317d52d9e725361e24cb5d8db44fee
mahmoud305 <ma5027300@gmail.com>
2022-03-30 15:46:36 +0200

    removing the data var from response

--- src/Task/Task.controller.js
+++ src/Task/Task.controller.js
@@ -7,8 +7,8 @@ const TaskModel = require("./Task.model");
         // we could save the date from here by adding it to the new task object -  date:Date.now() - or make it from the data base equals to the current date by default.
         const newTask= new TaskModel({varX:{varY: z}}); // initialize the new Job
         // console.log(newTask);
-        let data= newTask.save(); // saving the new job without waiting for it to get the reposne directly.
-        res.json({message:"success", data}) 
+        newTask.save(); // saving the new job without waiting for it to get the reposne directly.
+        res.json({message:"success, job added successfully"}) // the data will be empty
     } catch (error) {
         res.status(500).json({message:"Failed",error});
     }
